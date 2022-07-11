e1c570fbc219e7f019b77f40afffa205f2fe2819
Dima Svirsa <august4smile@gmail.com>
2022-07-11 13:44:08 +0300

    better view

--- task_todo/settings.py
+++ task_todo/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,6 +118,9 @@ DATETIME_INPUT_FORMATS = ('%Y-%m-%d %H:%M %p', )
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_URL = 'static/'
 
 # Default primary key field type
